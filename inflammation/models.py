bb492ee914ea1f88ae9c25e84550544702b95cfd
npetkov <nikola.petkov@ukaea.uk>
2024-06-03 15:49:15 +0100

    Added pylint and pytests

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row
+contains inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
@@ -17,6 +17,7 @@ def load_csv(filename):
     :param filename: Filename of CSV to load
     """
     return np.loadtxt(fname=filename, delimiter=',')
+
 
 def load_json(filename):
     """Load a numpy array from a JSON document.
@@ -39,7 +40,6 @@ def load_json(filename):
         return [np.array(entry['observations']) for entry in data_as_json]
 
 
-
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array."""
     return np.mean(data, axis=0)
@@ -53,4 +53,3 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
-
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,8 @@
 """Tests for statistics functions within the Model layer."""
-
+import os
 import numpy as np
 import numpy.testing as npt
-import os
+
 
 
 def test_daily_mean_zeros():
@@ -30,10 +30,12 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
 def test_load_from_json(tmpdir):
     from inflammation.models import load_json
     example_path = os.path.join(tmpdir, 'example.json')
     with open(example_path, 'w') as temp_json_file:
-        temp_json_file.write('[{"observations":[1, 2, 3]},{"observations":[4, 5, 6]}]')
+        temp_json_file.write('[{"observations":[1, 2, 3]}, \
+                             {"observations":[4, 5, 6]}]')
     result = load_json(example_path)
     npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
